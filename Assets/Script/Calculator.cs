ce627bfcf9b311a1b9d0a5c3618aa4c3cd6b94c7
TerrenceAddison <terrenceaddison32@gmail.com>
2022-11-24 18:42:50 +0800

    auto focus inputfield

--- Assets/Script/Calculator.cs
+++ Assets/Script/Calculator.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 public class Calculator : MonoBehaviour
-{   int primary,secondary,temp,final;
-    public GameObject Question,Result;
+{
+    int primary, secondary, temp, final;
+    public GameObject Question, Result;
     public TextMeshProUGUI PrimaryDigit, SecondaryDigit, SignDigit;
     public GameObject TMP_InputField_Answer;
-    public GameObject Correct,Wrong;
-    [SerializeField] TextMeshProUGUI countdownText; 
+    public GameObject Correct, Wrong;
+    [SerializeField] TextMeshProUGUI countdownText;
     float currentTime = 0f, startTime = 10f;
     bool onTime = true;
     // Start is called before the first frame update
@@ -24,30 +25,36 @@ public class Calculator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Z)){
-            CalculatorFn("addition");  
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            CalculatorFn("addition");
         }
-        if(Input.GetKeyDown(KeyCode.X)){
-            CalculatorFn("minus");  
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            CalculatorFn("minus");
         }
-        if(Input.GetKeyDown(KeyCode.C)){
-            CalculatorFn("multiplication");  
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            CalculatorFn("multiplication");
         }
-        if(Input.GetKeyDown(KeyCode.V)){
-            CalculatorFn("division");  
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            CalculatorFn("division");
         }
-        if(Input.GetKeyDown(KeyCode.B)){
-            CalculatorFn("modulus");  
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            CalculatorFn("modulus");
         }
-        if(Input.GetKeyDown(KeyCode.Return)){
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
             string answer = TMP_InputField_Answer.GetComponent<TMP_InputField>().text;
             Debug.Log("User Answer: " + answer);
-            CheckAnswerFn(final,answer);
+            CheckAnswerFn(final, answer);
         }
         currentTime -= 1 * Time.deltaTime;
         print(currentTime);
         countdownText.text = currentTime.ToString("0.0");
-        if(currentTime <= 0)
+        if (currentTime <= 0)
         {
             currentTime = 0;
             countdownText.color = Color.red;
@@ -58,41 +65,51 @@ public class Calculator : MonoBehaviour
         }
 
     }
-    public void CalculatorFn(string operation){
-        primary = UnityEngine.Random.Range(1,10);
-        secondary = UnityEngine.Random.Range(1,10);
+    public void CalculatorFn(string operation)
+    {
+        TMP_InputField_Answer.GetComponent<TMP_InputField>().Select();
+        TMP_InputField_Answer.GetComponent<TMP_InputField>().ActivateInputField();
+        primary = UnityEngine.Random.Range(1, 10);
+        secondary = UnityEngine.Random.Range(1, 10);
 
-        if(primary - secondary < 0){
+        if (primary - secondary < 0)
+        {
             temp = secondary;
             secondary = primary;
             primary = temp;
         }
-        if(operation == "addition"){
+        if (operation == "addition")
+        {
             final = primary + secondary;
             SignDigit.text = "+";
         }
-        if(operation == "minus"){
+        if (operation == "minus")
+        {
             final = primary - secondary;
             SignDigit.text = "-";
         }
-        if(operation == "multiplication"){
+        if (operation == "multiplication")
+        {
             final = primary * secondary;
             SignDigit.text = "*";
         }
-        if(operation == "division"){
+        if (operation == "division")
+        {
             final = primary / secondary;
             SignDigit.text = "/";
         }
-        if(operation == "modulus"){
+        if (operation == "modulus")
+        {
             final = primary % secondary;
             SignDigit.text = "%";
         }
-        
+
         PrimaryDigit.text = primary.ToString();
         SecondaryDigit.text = secondary.ToString();
-        Debug.Log("first: " + primary + " second: " + operation  + secondary + " final: " + final);
+        Debug.Log("first: " + primary + " second: " + operation + secondary + " final: " + final);
     }
-    public void CheckAnswerFn(int final, string answer){
+    public void CheckAnswerFn(int final, string answer)
+    {
         int user_answer = Convert.ToInt32(answer);
         // int.TryParse(inputField.text, out user_answer);
         // string user_answer = inputField.text;
@@ -102,19 +119,21 @@ public class Calculator : MonoBehaviour
         // } else {
         //     Debug.Log ("Not a valid int");
         // }
-        Debug.Log("user_answer: " + answer.GetType() + user_answer +answer + " "+ "final: " + final);
-        if(user_answer!=final){
+        Debug.Log("user_answer: " + answer.GetType() + user_answer + answer + " " + "final: " + final);
+        if (user_answer != final)
+        {
             Question.SetActive(false);
             Result.SetActive(true);
             Wrong.SetActive(true);
             Debug.Log("wrong");
         }
-        else if(user_answer==final){
+        else if (user_answer == final)
+        {
             Question.SetActive(false);
             Result.SetActive(true);
             Correct.SetActive(true);
             Debug.Log("Correct");
         }
     }
-    
+
 }
