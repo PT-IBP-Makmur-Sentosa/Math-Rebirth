40f94d855a38bed15b3918bd37b2bca342ade117
vickydarika <vickydarika4@gmail.com>
2022-11-26 03:52:07 +0800

    add fonts + mouse hover

--- Assets/Mainmenu.cs
+++ Assets/Mainmenu.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;  
 
 public class Mainmenu : MonoBehaviour
 {
+    public Sprite newButtonImage; 
+    public Button button;
+
     // Start is called before the first frame update
     void Start()
     {
