5d47ca4cc9d3688270c368333276315620c0a9c5
Lewisu <gsc005@gmail.com>
2022-11-27 17:36:06 +0800

    Trait Sprites Change
    Animation of each trait changed to still sprite of each trait

--- Assets/TraitManager.cs
+++ Assets/TraitManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class TraitManager : MonoBehaviour
@@ -9,6 +10,8 @@ public class TraitManager : MonoBehaviour
     [SerializeField] TextMeshProUGUI traitInfo;
     [SerializeField] string[] Tname;
     [SerializeField] Animator anim;
+    [SerializeField] Image sprites;
+    [SerializeField] Sprite[] img;
     string[] Tinfo = new string[4];
     
     
@@ -18,19 +21,20 @@ public class TraitManager : MonoBehaviour
     {
         Tinfo[0] = @"Having a hobby of going to the gym during his leisure time. He has a very healthy body and trained muscle. Someone saw him lifting a car by himself, but that itself is just a rumour.
 
-Trait focuses on Strength, +5 Str per Level & +2 for other stats.";
+Trait focuses on Strength, +5 Str per Level & +2 for other Stats.";
         Tinfo[1] = @"He was once the ace of his university track and field team. He has a very good agility and quick. It was rumoured that he's as fast as Usain Bolt.
 
-Trait focuses on Agility, +5 Agi per Level & +2 for other stats.";
+Trait focuses on Agility, +5 Agi per Level & +2 for other Stats.";
         Tinfo[2] = @"Reading is the same as breathing for him. He has been reading lots of books since young age, that being said, he knows a lot of knowledge thus having a sharper mind than the average human. Some suspect he might be the reincarnation of Albert Einstein.
 
-Trait focuses on Intelligent, +5 Int per Level & +2 for other stats.";
+Trait focuses on Intelligent, +5 Int per Level & +2 for other Stats.";
         Tinfo[3] = @"Not much can be said about this guy. He is just your ordinary Joe you can find anywhere. Not sure how did he get into this mess.
 
-Trait is equally distributed, +3 for all stats per level.";
+Trait is equally distributed, +3 for all Stats per Level.";
 
         traitName.text = Tname[0];
         traitInfo.text = Tinfo[0];
+        sprites.sprite = img[0];
     }
 
     // Update is called once per frame
@@ -46,6 +50,7 @@ Trait is equally distributed, +3 for all stats per level.";
         else idx -= 1;
         traitName.text = Tname[idx];
         traitInfo.text = Tinfo[idx];
+        sprites.sprite = img[idx];
         anim.SetInteger("index", idx);
     }
     public void nextTrait()
@@ -54,6 +59,7 @@ Trait is equally distributed, +3 for all stats per level.";
         else idx += 1;
         traitName.text = Tname[idx];
         traitInfo.text = Tinfo[idx];
+        sprites.sprite = img[idx];
         anim.SetInteger("index", idx);
     }
 }
