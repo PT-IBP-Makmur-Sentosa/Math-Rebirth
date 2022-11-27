4fa4c3b9d506940a49b1d5e08a948dca63cc2f8f
vickydarika <vickydarika4@gmail.com>
2022-11-27 14:43:14 +0800

    gedein canvas

--- Assets/Mainmenu.cs
+++ Assets/Mainmenu.cs
@@ -6,13 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class Mainmenu : MonoBehaviour
 {
-    public Sprite newButtonImage; 
-    public Button button;
+    public GameObject menu;
+    public GameObject help;
+    public GameObject help1;
+    public GameObject help2;
+    public GameObject setting;
+    public GameObject about;
+    // public Canvas menuScreens;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        menu.SetActive(true);
     }
 
     // Update is called once per frame
@@ -28,13 +33,32 @@ public class Mainmenu : MonoBehaviour
         Application.Quit(); 
     }  
     public void About() {  
-        SceneManager.LoadScene("About"); 
+        about.SetActive(true);
+        menu.SetActive(false);
     }  
     public void Setting() {  
-        SceneManager.LoadScene("Setting"); 
+        setting.SetActive(true);
+        menu.SetActive(false);
     } 
     public void Help() {  
-        SceneManager.LoadScene("Help"); 
+        help.SetActive(true);
+        menu.SetActive(false);
     }   
+    public void Help1() {  
+        help1.SetActive(true);
+        help.SetActive(false);
+    }  
+    public void Help2() {  
+        help2.SetActive(true);
+        help1.SetActive(false);
+    }  public void backmenu() {  
+        menu.SetActive(true);
+        help.SetActive(false);
+        help1.SetActive(false);
+        help2.SetActive(false);
+        about.SetActive(false);
+        setting.SetActive(false);
+
+    }  
 }
 
