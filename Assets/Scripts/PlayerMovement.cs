ce7557997e55f85b37ce6b6e52942c26d153efd0
Lewisu <gsc005@gmail.com>
2022-12-20 18:55:23 +0800

    Small Tweak

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -40,6 +40,7 @@ public class PlayerMovement : MonoBehaviour
     GlobalControl globalcontrol;
     public List<GameObject> unityGameObjects = new List<GameObject>();
     public List<string> StringTagEnemy = new List<string>() {"Shade","Skeleton"};
+    [SerializeField] AudioClip clips;
 
     // Start is called before the first frame update
     void Start()
@@ -233,6 +234,7 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.name == "Soul")
         {
             print("Soul collected");
+            gameObject.GetComponent<AudioSource>().PlayOneShot(clips);
             GameObject glob = GameObject.Find("GlobalObject");
             glob.GetComponent<GlobalControl>().playerCurrency = GameObject.Find("CombatManager").GetComponent<CombatManager>().soulCurrency;
             GameObject.Find("CombatManager").GetComponent<CombatManager>().soulCurrency = 0;
