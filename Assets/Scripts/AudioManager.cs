e6e0a1560c6b5bf4deedf76a60383ff3dc2c150d
Bryan Hartono <bryanhartono03@gmail.com>
2023-01-07 13:55:30 +0800

    fixed audio

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
- 
     [SerializeField] AudioClip[] audioClip;
-    public float soundToPlay = -1.0f; //this with designate which sound to play. -1 means don't play any sound. This is a float because then the animation window can access it. public AudioClip[] audioClip; //this holds the sounds
+    [SerializeField] int soundToPlay = -1;
+    private bool isPlaying;
+    public AudioSource sfx;
 
-    // AudioSource audio;//for holding the audio source
+    void Update ()
+    { 
 
-
-
-    void Update () { 
-    //put the following in update if (soundToPlay > -1.0f)}
-    //if the sound is greater than the value for not playing a sound PlaySound((int) soundToPlay, 1);
-    //play the sound, casting the float to an int so that the audio source can use it soundToPlay = -1.0f;
-    //set it back to zero to keep this from looping back around and playing the sound again. } }
-     if (soundToPlay > -1.0f){
-         PlaySound((int)soundToPlay, 1);
-         soundToPlay = -1.0f;
-     }
+        if (soundToPlay != -1 && !isPlaying)
+        {
+            sfx.clip = audioClip[soundToPlay];
+            sfx.Play();
+            isPlaying = true;
+        }
+        else if(soundToPlay == -1)
+        {
+            isPlaying = false;
+            sfx.clip = null;
+        }
     }
-    void PlaySound(int clip, float volumeScale){ 
-        Debug.Log("audioclip"+audioClip[clip]);
-        Debug.Log("clip int "+clip);
-
-        GetComponent<AudioSource>().PlayOneShot(audioClip[clip], volumeScale);
-    }
-        //play the sound with the designated clip and volume scale }
 }
