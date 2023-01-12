3d51e22b5bb1fff7f132d98d044e59413d5fab92
vickydarika <vickydarika4@gmail.com>
2023-01-12 16:47:52 +0800

    push prefab tutorial

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -9,6 +9,8 @@ public class Tutorial : MonoBehaviour
     public GameObject move_tutorial;
     public GameObject inventory_tutorial;
     public GameObject combat_tutorial;
+    public GameObject skill_tutorial;
+    public GameObject charpage_tutorial;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +18,15 @@ public class Tutorial : MonoBehaviour
         move_tutorial = GameObject.Find("MoveTutorial");
         inventory_tutorial = GameObject.Find("InventoryTutorial");
         combat_tutorial = GameObject.Find("inCombatTutorial");
+        skill_tutorial = GameObject.Find("Skill page tutorial");
+        charpage_tutorial = GameObject.Find("charpage");
 
         tutorial_background.SetActive(true);
         move_tutorial.SetActive(true);
         inventory_tutorial.SetActive(false);
         combat_tutorial.SetActive(false);
+        skill_tutorial.SetActive(false);
+        charpage_tutorial.SetActive(false);
     }
 
     // Update is called once per frame
