4489a9d67ed675b2a16d07786a666ae0d440a75b
Edoardo Dominikus, 蔣增豐 <79357023+edoardodominikus@users.noreply.github.com>
2023-01-13 18:56:16 +0800

    Update spikes_hurt.cs

--- Assets/Scripts/spikes_hurt.cs
+++ Assets/Scripts/spikes_hurt.cs
@@ -7,12 +7,16 @@ public class spikes_hurt : MonoBehaviour
     // Start is called before the first frame update
     public string unitName;
     public Unit playerUnit;
-
+    private bool TrapHit;
+    public AudioSource hit;
     void Start()
     {
         // if (gameObject.tag == "Player") currentHP = GameObject.Find("GlobalObject").GetComponent<GlobalControl>().playerCurrentHP;
     }
-
+    void Update()
+    {
+        
+    }
     // Update is called once per frame
     
     void OnTriggerEnter2D(Collider2D other) { 
@@ -20,9 +24,24 @@ public class spikes_hurt : MonoBehaviour
         if(other.CompareTag("Player")) {
             print("Another object has entered the trigger");  
             if (playerUnit.currentHP >= playerUnit.maxHP * 0.10f ) {
-                playerUnit.currentHP -= playerUnit.maxHP * 0.05f ;
+                TrapHit = true;
+                StartCoroutine(CastDamage(0.05f));
             }
             
         }
     } 
+    void OnTriggerExit2D(Collider2D other) { 
+        if(other.CompareTag("Player")) {
+            TrapHit = false;
+        }
+    } 
+    IEnumerator CastDamage(float damage){
+        while(TrapHit){
+            playerUnit.currentHP -= playerUnit.maxHP * 0.05f ;
+            hit.Play();
+            yield return new WaitForSeconds(1.0f);
+        }
+        
+    }
+
 }
